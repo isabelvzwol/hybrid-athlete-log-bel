5242c9567485461967ccd4bb35d4a297614c817c
isabelvzwol <isabelvanzwol@outlook.com>
2026-09-22 14:53:06 +0200

    Create Gezondheid tab for health tracking
    Added a new Gezondheid tab to track complaints and body weight.

---
+++ src/screens/Gezondheid.jsx
@@ -0,0 +1,16 @@
+/* ---------------- Gezondheid-tab ----------------
+   Nieuw tabblad (was niet in het origineel): bundelt klachten & blessures
+   (verplaatst vanaf Home) en lichaamsgewicht op één plek, met ruimte om er
+   later nog gezondheidsmetrics aan toe te voegen. Stemming blijft bewust op
+   Home staan, dat is een losse keuze geweest. */
+import React from 'react';
+import { ComplaintTracker, BodyWeightTracker } from '../components/health.jsx';
+var e = React.createElement;
+
+export function GezondheidTab(props) {
+  var s = props.state;
+  return e('div', { className: 'flex flex-col gap-5' },
+    e(BodyWeightTracker, { bodyWeightLogs: s.bodyWeightLogs, onSave: props.saveBodyWeight, onDelete: props.deleteBodyWeight }),
+    e(ComplaintTracker, { complaintLogs: s.complaintLogs, onAdd: props.addComplaint, onDelete: props.deleteComplaint })
+  );
+}
